85e1edd3c42a55d945cb71614c8d522cc68b15a4
Dawson Botsford <3408480+dawsbot@users.noreply.github.com>
2017-01-31 00:55:27 -0800

    :metal: Bug fixes, Ready to Open-Source

--- src/cli.js
+++ src/cli.js
@@ -11,16 +11,10 @@ const Pluc = require('./');
 const okayHand = '👌';
 const cli = meow(
   `Usage
-    $ pluc <new alias> <command>
+    $ pluc <alias> <command>
     or
-    $ pluc <new alias>
+    $ pluc <alias>
     # Command is taken from most-recently executed in history
-
-  Options
-    --shellPath  Print path to compiled shell
-    --jsonPath   Print path to json source
-    --transpile  Manually transpile json to shell (You probably won't need)
-    --projectName Manually change projectName (You probably won't need)
 
   Examples
     $ pluc gpom "git push origin master"
@@ -29,26 +23,35 @@ const cli = meow(
     $ npm install
     $ pluc ni
       Aliased "ni" to "npm install"
+
+  Options
+    --shellPath  Print file path to compiled shell
+    --jsonPath   Print file path to json source
+    --transpile  Manually transpile json to shell (You probably won't need)
+    --projectName Manually change projectName (You probably won't need)
     `
 );
 
 updateNotifier({pkg: cli.pkg}).notify();
 
-const logSuccess = msg => console.log(chalk.green(msg));
+const logSuccess = message => console.log(chalk.green(`${okayHand}  ${message} ${okayHand}`));
 const flags = cli.flags;
 const hasInput = cli.input.length > 0;
 const hasFlags = Object.keys(cli.flags).length !== 0;
 
 let pluc = new Pluc();
 
-// TODO: on postinstall, setup empty shellPath
 if (!(hasInput || hasFlags)) {
   console.log(cli.help);
 }
 
 if (hasFlags) {
   // TODO: have transpile message show where it transpiled out to
-  flags.transpile && pluc.transpileJson() && logSuccess('transpiled with success');
+  if (flags.transpile) {
+    const message = `Transpiled ${pluc.jsonPath} to ${pluc.shellPath}`;
+    pluc.transpileJson();
+    logSuccess(message);
+  }
 
   const projectName = flags.projectName;
   projectName && (pluc = new Pluc({projectName}));
@@ -62,7 +65,7 @@ if (hasInput) {
   const command = cli.input[1] || pluc.lastCommand;
   // TODO: if alias already exists, prompt to save over
   pluc.setAlias(alias, command);
-  logSuccess(`${okayHand}  Aliased "${alias}" to "${command}" ${okayHand}`);
+  logSuccess(`Aliased "${alias}" to "${command}"`);
 
   pluc.transpileJson();
 }
